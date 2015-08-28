8063d3d6d24149d64486852824fa7341640a16c6
Aw Wei Lin <weinilinhappy@gmail.com>
2015-08-28 23:48:25 +0800

    treat main class as Input

--- KWICKWACKWOC/src/KWICKWACKWOC.java
+++ KWICKWACKWOC/src/KWICKWACKWOC.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * 
@@ -9,14 +11,39 @@
  */
 public class KWICKWACKWOC {
 
+	private static Scanner sc;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Input input = new Input();
-		input.run();
-		
+		runForInput();	
 
+	}
+	
+	public static void runForInput() {
+		ArrayList<String> listOfLines = new ArrayList<String>();
+		ArrayList<String> listOfIgnoredWord = new ArrayList<String>();
+		
+		System.out.printf("Enter the title:" + "\n" + "(click enter twice to exit) " + "\n");
+		sc = new Scanner(System.in);	
+		takeInInputs(listOfLines);
+		
+		System.out.printf("Enter the word to be ignored: " + "\n" + "(click enter twice to exit)" + "\n");
+		takeInInputs(listOfIgnoredWord);
+		
+		new InputData(listOfIgnoredWord, listOfLines);
+	}
+
+	private static void takeInInputs(ArrayList<String> listOfLines) {
+		while (true) {
+			String lines = sc.nextLine();
+			
+			if (lines.equals("") || lines.equals("exit")) {
+				break;
+			}
+			
+			listOfLines.add(lines);		
+		}
 	}
 
 }
